03492d35ee9dfb277ba50c9846c64d8c88952f5a
TUMMALAPALLI1993 <tummalapalli.kirankumar@gmail.com>
2018-04-17 12:13:05 +0200

    Add project files.

---
+++ MDE2018/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MDE2018.Models;
+
+namespace MDE2018.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            Student student = new Student();
+            student.Firstname = "Tummalapalli";
+            student.Lastname = "Kiran Kumar";
+            student.DOB = new DateTime(1993, 11, 23);
+
+            //student.PictureURL = @"~\Home\Kiran(1).jpg";
+            //student.PictureURL = "Views/Home/Kiran(1).jpg";
+            student.PictureURL = "/images/Kiran(1).jpg";
+
+
+            return View(student);
+        }
+
+        public IActionResult AllStudents()
+        {
+            List<string> name = new List<string>();
+
+            name.Add("Kiran");
+            name.Add("Xander");
+            name.Add("Vishnu");
+            name.Add("Roy");
+            name.Add("Paulina");
+            name.Add("Kristen");
+
+
+            return View(name);
+        }
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MDE2018/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MDE2018.Models
+{
+    public class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DOB { get; set; }
+        public string PictureURL { get; set; }
+    }
+}
